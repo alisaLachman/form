a73d7ff901cd2df175cc5d8b143d8316cec280a3
alisaLachman <alisal@wix.com>
2024-07-09 12:31:27 +0300

    changed properties

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -6,18 +6,15 @@ import { Dropdown } from '../../components/dropdown/dropdown';
 import { Button } from '../../components/button/button';
 import { Header } from '../../components/header/header';
 
-
-
 export interface FormProps {
     className?: string;
 }
-
 
 export const Form = ({ className }: FormProps) => {
     return (
         <div className="formContainer">
             <div className="formHeader">
-                <Header />
+                <Header userNumber={100} userNumbertext="hundred" />
             </div>
             <div className="userName">
                 <Input color="yellow" title="Name" />
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,14 @@
 import classNames from 'classnames';
 import styles from './header.module.scss';
 
-const userNumbertext = "Two Hundred";
 
 export interface HeaderProps {
     className?: string;
     userNumber?: number;
+    userNumbertext?: string;
 }
 
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
- */
+
 export const Header = ({ className }: HeaderProps) => {
     return (
         <div className={classNames(styles.root, className)}>
